311dc43269d6f7b8f7af79150d872227cf6e83ca
Giorgi <giorgi@giorgi.dev>
2020-09-24 16:08:09 +0400

    New Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OktaAspNetIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
